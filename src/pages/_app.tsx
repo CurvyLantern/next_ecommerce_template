1d26e8a20c7ff1468f4e579c181c47ac3e990c5b
unknown <ashfaqnaseem1@gmail.com>
2023-03-21 22:28:31 +0600

    bug fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,28 @@
 import '@/styles/globals.css';
 
-import { inter, roboto } from '@/fonts';
 import { NotificationWrapper } from '@/libs/toast';
 import type { AppProps } from 'next/app';
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <style jsx global>
+      {/* Probably this is causing the error .. neeeds to install client bla bla */}
+      {/* <style jsx global>
         {`
           :root {
             --font-inter: ${inter.style.fontFamily};
             --font-roboto: ${roboto.style.fontFamily};
           }
         `}
-      </style>
+      </style> */}
+      {/* Possible fix */}
+      {/* <style
+        dangerouslySetInnerHTML={{
+          _html: `
+            :root {
+              --font-inter: ${inter.style.fontFamily}
+            }
+        `, */}
+      {/* }}></style> */}
       <Component {...pageProps} />
       <NotificationWrapper />
     </>
