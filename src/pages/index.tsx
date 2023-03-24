8eae138dd6bb0d9f6d7e64860cce8e6d2dfe63a8
Tuhin Hossain <95519628+tuhin180@users.noreply.github.com>
2023-03-25 00:34:35 +0600

    done

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
-import { notify } from '@/libs/toast';
-import { GetServerSideProps, NextPage } from 'next/types';
+import { notify } from "@/libs/toast";
+import { GetServerSideProps, NextPage } from "next/types";
 
 export const getServerSideProps: GetServerSideProps<{
   title: string;
@@ -7,7 +7,7 @@ export const getServerSideProps: GetServerSideProps<{
   return {
     props: {
       title:
-        'Hi there this is a basic next js page rendered with data from server',
+        "Hi there this is a basic next js page rendered with data from server",
     },
   };
 };
@@ -18,40 +18,43 @@ interface HomePageProps {
 }
 const HomePage: NextPage<HomePageProps> = ({ title }) => {
   return (
-    <div className='bg-white py-10 h-screen text-center'>
-      <h1 className='text-blue-400 text-5xl my-10'>{title}</h1>
+    <div className="bg-white py-10 h-screen text-center">
+      <h1 className="text-blue-400 text-5xl my-10">{title}</h1>
 
-      <header className='text-3xl'>
-        <h2 className='font-roboto'>
+      <header className="text-3xl">
+        <h2 className="font-roboto">
           Roboto font : server side rendered by next font.
         </h2>
-        <h2 className='font-inter'>
+        <h2 className="font-inter">
           Inter font : server side rendered by next font.
         </h2>
       </header>
-      <p className='text-xl p-2 text-black'>Here are some Daisy ui buttons</p>
+      <p className="text-xl p-2 text-black">Here are some Daisy ui buttons</p>
 
-      <div className='flex items-center justify-around '>
+      <div className="flex items-center justify-around ">
         <button
           onClick={() => {
-            notify('hey there i am a toast ');
+            notify("hey there i am a toast ");
           }}
-          className='btn btn-outline btn-info'>
+          className="btn btn-outline btn-info"
+        >
           Info
         </button>
         <button
           onClick={() => {
-            notify.success('hey there i am a toast ');
+            notify.success("hey there i am a toast ");
           }}
-          className='btn btn-outline btn-success'>
+          className="btn btn-outline btn-success"
+        >
           Success
         </button>
-        <button className='btn btn-outline btn-warning'>Warning</button>
+        <button className="btn btn-outline btn-warning">Warning</button>
         <button
           onClick={() => {
-            notify.error('hey there i am a toast ');
+            notify.error("hey there i am a toast ");
           }}
-          className='btn btn-outline btn-error'>
+          className="btn btn-outline btn-error"
+        >
           Error
         </button>
       </div>
@@ -60,4 +63,3 @@ const HomePage: NextPage<HomePageProps> = ({ title }) => {
 };
 
 export default HomePage;
-
