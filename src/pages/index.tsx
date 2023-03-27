71f649ea7d79b2f910db1fb9795602d4ab0f292e
naseem(T_T) <ashfaqnaseem1@gmail.com>
2023-03-27 12:44:36 +0600

    removed serversideProps and implemented footer,header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,63 +1,43 @@
+import Container from '@/components/containers/Container';
+import Footer from '@/components/Footers/Footer';
+import Header from '@/components/Headers/Header';
 import PrimaryProductCard from '@/components/PrimaryProductCard';
-import { notify } from '@/libs/toast';
-import { GetServerSideProps, NextPage } from 'next/types';
-
-export const getServerSideProps: GetServerSideProps<{
-	title: string;
-}> = async () => {
-	return {
-		props: {
-			title: 'Hi there this is a basic next js page rendered with data from server',
-		},
-	};
-};
+import { NextPage } from 'next/types';
 
 // typescript interface
 interface HomePageProps {
-	title: string;
+  title: string;
 }
-const HomePage: NextPage<HomePageProps> = ({ title }) => {
-	return (
-		// <div className='bg-white py-10 h-screen text-center'>
-		//   <h1 className='text-blue-400 text-5xl my-10'>{title}</h1>
+const HomePage: NextPage<HomePageProps> = () => {
+  return (
+    <>
+      {/* a common header  */}
+      <Header />
 
-		//   <header className='text-3xl'>
-		//     <h2 className='font-roboto'>
-		//       Roboto font : server side rendered by next font.
-		//     </h2>
-		//     <h2 className='font-inter'>
-		//       Inter font : server side rendered by next font.
-		//     </h2>
-		//   </header>
-		//   <p className='text-xl p-2 text-black'>Here are some Daisy ui buttons</p>
+      {/* Slider */}
+      <div className='h-72 bg-gray-800 text-center text-white'>
+        Slider will go here
+      </div>
 
-		//   <div className='flex items-center justify-around '>
-		//     <button
-		//       onClick={() => {
-		//         notify('hey there i am a toast ');
-		//       }}
-		//       className='btn btn-outline btn-info'>
-		//       Info
-		//     </button>
-		//     <button
-		//       onClick={() => {
-		//         notify.success('hey there i am a toast ');
-		//       }}
-		//       className='btn btn-outline btn-success'>
-		//       Success
-		//     </button>
-		//     <button className='btn btn-outline btn-warning'>Warning</button>
-		//     <button
-		//       onClick={() => {
-		//         notify.error('hey there i am a toast ');
-		//       }}
-		//       className='btn btn-outline btn-error'>
-		//       Error
-		//     </button>
-		//   </div>
-		// </div>
-		<PrimaryProductCard />
-	);
+      {/* show collection */}
+      <Container className='h-52 my-10 bg-gray-800 text-center text-white'>
+        Various collections
+      </Container>
+
+      {/* TODO: this is just for demo purposes */}
+      {/* show Demo products */}
+      <div>
+        {Array(20)
+          .fill(0)
+          .map((_, index) => {
+            return <PrimaryProductCard key={index} />;
+          })}
+      </div>
+
+      {/* a common footer */}
+      <Footer />
+    </>
+  );
 };
 
 export default HomePage;
