59580de8350d2356d2e3b6d0fccf667b90c3d381
naseem(T_T) <ashfaqnaseem1@gmail.com>
2023-03-28 00:52:48 +0600

    silly import mistake

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,26 +1,26 @@
 import '@/styles/globals.css';
 
-import { NotificationWrapper } from '@/libs/toast';
+import { ToastWrapper } from '@/libs/toast';
 import type { AppProps } from 'next/app';
 
 import { inter, roboto } from '@/fonts';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <style
-        dangerouslySetInnerHTML={{
-          __html: `
+	return (
+		<>
+			<style
+				dangerouslySetInnerHTML={{
+					__html: `
           :root {
             --font-inter: ${inter.style.fontFamily};
             --font-roboto: ${roboto.style.fontFamily};
           }
         `,
-        }}
-      />
-      <Component {...pageProps} />
-      <NotificationWrapper />
-    </>
-  );
+				}}
+			/>
+			<Component {...pageProps} />
+			<ToastWrapper />
+		</>
+	);
 }
 
